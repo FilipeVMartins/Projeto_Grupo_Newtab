38c4604debc06036ccb64478b7d0569acd12a128
FilipeVMartins <filipe_2021@hotmail.com>
2021-05-15 13:26:57 -0300

    added hero, search input, results title to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,20 +2,43 @@ import React from 'react';
 
 import './Home.css';
 
+import { ReactComponent as SearchIcon } from '../../images/layout1/icon-search.svg';
+
 export default class Home extends React.Component {
 
   render() {
-
+    const logo = require('../../images/layout1/hero-bg-mobile.jpg'); // with require
 
     return (
       <div className="home-content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,</p>
+
+        <div className="home-header">
+          <div className="home-nav" >
+          </div>
+
+          <div className="home-title-input-wrapper">
+            <div className="home-header-title">
+              <h1>Encontre hashtags de maneira fácil</h1>
+              <h3>Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo</h3>
+            </div>
+
+            <div className="home-input-wrapper">
+              <SearchIcon className="search-icon" />
+              <form>
+                <input placeholder="Buscar..."></input>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className="home-results">
+          <div className="results-title">
+            <h2>Exibindo os 10 resultados mais recentes para #<span>natureza</span></h2>
+          </div>
+
+        </div>
       </div>
     );
   };
