1ee9a19f71d403c25030680f83ba48b3e262b7ed
David W de Jesus <davidwj15@gmail.com>
2021-05-15 19:26:53 -0300

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer(){
+
+    return(
+        <div className="footerBackground">
+            <span className="footerText">@Cocreare 2020. Todos os direitos reservados</span>
+        </div>
+    )
+}
+export default Footer;
