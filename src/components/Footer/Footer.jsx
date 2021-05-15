3c72d82518485a6634beb339ab6bd0b84833a31c
David W de Jesus <davidwj15@gmail.com>
2021-05-15 20:01:14 -0300

    chenged footer, added windows dimension

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./Footer.css";
+import useWindowDimensions from "../WindowsDimension/useWindowDimensions"
+
 
 function Footer(){
-
+    const {width} = useWindowDimensions();
+    const platform =  width>1000 ? "Desktop" : "Mobile";
     return(
         <div className="footerBackground">
-            <span className="footerText">@Cocreare 2020. Todos os direitos reservados</span>
+            <span className={"footerText footerText"+platform}>
+            @Cocreare 2020. Todos os direitos reservados
+            </span>
         </div>
     )
 }
