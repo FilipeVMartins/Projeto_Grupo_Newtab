6914b450aad92362e7927d9319f0da900fc44f36
FilipeVMartins <filipe_2021@hotmail.com>
2021-05-26 21:31:47 -0300

    just white spaces

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -22,7 +22,7 @@ export default class Home extends React.Component {
 
 
   state = {
-    searchedString: 'arvore',
+    searchedString: 'natureza',
     twitterImages: null,
     twitterPosts: null,
 
